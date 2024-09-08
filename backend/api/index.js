0d381299cea477f2d71324f494780e9e12849021
AkshayTripathi0331 <akshaytripathi630@gmail.com>
2024-09-08 19:11:39 +0530

    Added vercel configuration for backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 const connectDB = require('../config/db');
 const contactRoutes = require('../routes/contactRoutes');
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 connectDB();
-
+console.log(process.env.MONGO_URI);
 const app = express();
 app.use(express.json());
 
@@ -13,3 +13,7 @@ app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+module.exports = app;
